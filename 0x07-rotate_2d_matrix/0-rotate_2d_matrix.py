20f617dbacf76849dfd2388dd48aead5be0d255c
BrianKvin <briankvin@gmail.com>
2023-08-17 22:18:41 +0300

    rotate matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 # rotate a matrix by 90 degrees
 
-#def rotate_2d_matrix(matrix):
-    
-    # reverse the matrix
+# def rotate_2d_matrix(matrix):
+
+#    reverse the matrix
 #    for i in range(len(matrix)):
 #        matrix[i].reverse()
 
-    # transpose of the matrix
+# transpose of the matrix
 #    for i in range(len(matrix)):
 #        for j in range(i, len(matrix)):
 
-            # swapping matrix[i][j] and mat[j][i]
+# swapping matrix[i][j] and mat[j][i]
 #            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
 # Function to print the matrix
-#def displayMatrix(matrix):
+# def displayMatrix(matrix):
 
 #    for i in range(0, len(matrix)):
 #        for j in range(0, len(matrix)):
@@ -23,7 +23,7 @@
 #        print()
 
 # Driver code
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    matrix = [[1, 2, 3],
 #            [4, 5, 6],
 #            [7, 8, 9]]
